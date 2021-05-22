09bdf80be8c77a30710cb86b0b229b2ff05423ae
David <davidb7@bushlabs.com>
2021-05-21 23:41:48 -0500

    feat: initial setup for bushlabs using nuxt

---
+++ nuxt.config.js
@@ -0,0 +1,49 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'Bush Labs',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'mobile-web-app-capable', content: 'yes' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'author', content: 'David Bush' },
+    ],
+    link: [
+      { rel: 'authorization_endpoint', href: 'https://indieauth.com/auth' },
+      { rel: 'token_endpoint', href: 'https://tokens.indieauth.com/token' },
+      { rel: 'microsub', href: 'https://aperture.p3k.io/microsub/557' },
+    ],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    // https://go.nuxtjs.dev/stylelint
+    '@nuxtjs/stylelint-module',
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+}
---
+++ pages/index.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <nav class="flex items-center justify-between bg-blue-900 p-6">
+      <div class="flex items-center flex-shrink-0 mr-6 text-white text-xl">
+        Bush Labs
+      </div>
+    </nav>
+
+    <div class="mt-5 mx-16 px-2">
+      <div class="mb-5">
+        <h2 class="text-2xl text-gray-800">About Me</h2>
+        <div class="ml-3">
+          <p>Hi, my name is <span class="p-name">David Bush</span>.</p>
+          This is really just a place for me to experiment and explore some web
+          concepts and to keep track of a few things. See the "Stuff I Like" and
+          "Todo" sections below.
+        </div>
+      </div>
+
+      <div class="mb-5">
+        <h2 class="text-2xl text-gray-800">Currently Working On</h2>
+        <ul class="ml-2">
+          <li><code>[*]</code> Switching bushlabs.com to use Nuxt</li>
+          <li><code>[*]</code> Switching bushlabs.com to use Tailwind CSS</li>
+        </ul>
+      </div>
+
+      <div class="mb-5">
+        <h2 class="text-2xl text-gray-800">Todo</h2>
+        <ul class="ml-2">
+          <li><code>[ ]</code> Add email to find me</li>
+          <li><code>[ ]</code> Add items to "Stuff I Like" section</li>
+        </ul>
+      </div>
+
+      <div class="mb-5">
+        <h2 class="text-2xl text-gray-800">Stuff I Like</h2>
+        <ul class="ml-3">
+          <li>
+            <a
+              href="https://indieweb.org"
+              class="text-blue-600 hover:underline hover:text-blue-900"
+              >IndieWeb</a
+            >
+          </li>
+          <li>
+            <a
+              href="https://github.com/philc/vimium"
+              class="text-blue-600 hover:underline hover:text-blue-900"
+              >Vimium</a
+            >
+          </li>
+          <li>
+            <a
+              href="https://bonsaimirai.com/"
+              class="text-blue-600 hover:underline hover:text-blue-900"
+              >Mirai</a
+            >
+          </li>
+        </ul>
+      </div>
+
+      <div class="mb-5">
+        <h2 class="text-2xl text-gray-800">Find Me</h2>
+        <div class="ml-3">
+          <p>You may find me here:</p>
+          <ul name="me">
+            <li>
+              <a
+                class="text-blue-600 hover:underline hover:text-blue-900"
+                href="https://github.com/davidb7"
+                rel="me"
+                >@davidb7</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
